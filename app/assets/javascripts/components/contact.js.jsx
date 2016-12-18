5b353daf080b1e3b8d77c4b5dff0eb508afd58ac
Sunjay <lorenzianheteroceran@gmail.com>
2016-12-18 10:49:05 +0000

    Added send to server method with ajax

--- app/assets/javascripts/components/contact.js.jsx
+++ app/assets/javascripts/components/contact.js.jsx
@@ -5,8 +5,29 @@ var ContactForm = React.createClass({
     event.preventDefault();
     console.warn('submit has been triggered');
     
-    //Submit to server and render success message to user
+    // Get data from form
     
+    // Send data to server
+    
+    
+    
+  },
+  
+  sendToServer: function(formData){
+    var promise = $.ajax({
+      url: '/contact',
+      method: "POST",
+      data: formData,
+      dataType: 'JSON'
+    });
+    
+    promise.done(function(){
+      alert("Contact submitted!");
+    });
+    
+    promise.fail(function(jqXHR,  textStatus,  errorThrown) {
+      alert("Could not submit because" + errorThrown)
+    });
   },
 
   render: function(){
